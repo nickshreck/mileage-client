b48ca45764600830bc0ff466db3e199c83235fd2
nickshreck <nickshreck@gmail.com>
2022-11-11 13:39:51 +0000

    devserver

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,11 @@ module.exports = {
   devServer: {
     port: 3000,
     historyApiFallback: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+    }
   },
 
   module: {
